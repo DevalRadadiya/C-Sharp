f87ff7b3b5d209fb1da1d8d05059bb1c130b7526
Deval Radadiya <108853532+DevalRadadiya@users.noreply.github.com>
2023-10-06 12:58:42 +0530

    Add files via upload

---
+++ WindowsFormsApplication8/WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace WindowsFormsApplication8
+{
+    
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection(@"D:\Sem5\C#\Program\WindowsFormsApplication8\WindowsFormsApplication8\Form1.cs");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
